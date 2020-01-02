5893cc454bfcd4286e01346561ca5ce29e838d69
Olivia <oliviacoumans7@gmail.com>
2020-01-02 22:01:26 +0100

    Configure eslint prettier and typescript

---
+++ src/example.js
@@ -0,0 +1,12 @@
+const username = 'freddy';
+typeof username === 'string';
+
+if (!('serviceWorker' in navigator)) {
+  // you have an old browser 🤷‍♂️
+}
+
+const greeting = 'hello';
+
+console.log(`${greeting} world!`);
+
+[1, 2, 3].forEach(x => console.log(x));
